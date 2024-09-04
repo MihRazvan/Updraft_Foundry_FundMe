961d5151cb8fe4d3343326839b6ab27448224ae9
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-09-04 11:34:20 +0300

    setup deploy script

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "../src/FundMe.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
+
+contract DeployFundMe is Script {
+    function run() public {
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe();
+        vm.stopBroadcast();
+    }
+}
