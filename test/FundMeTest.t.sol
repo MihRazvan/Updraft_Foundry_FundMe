8a47c1c1ceb4108d0a96719e1fbd2f9916e95914
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-09-04 15:15:34 +0300

    update tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,9 +7,22 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address testAddress = makeAddr("testAddress");
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert();
+        fundMe.fund(){value: 0 ether};
+    }
+
+    function testFundUpdatesDataStructure() public {
+        vm.prank(testAddress);
+        vm.deal(testAddress, 3 ether);
+        fundMe.fund{value: 1 ether}();
+        assertEq(fundMe.getAddressToAmountFunded(testAddress), 1 ether);
     }
 }
